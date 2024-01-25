8d2616cc0c525531f1c1d1bb1603e5c5c244b110
Liam Rahum <liam@rahum.co.il>
2024-01-25 10:52:05 +0200

    Feat: added Subscriptions class

--- lib/classes/subscriptions.dart
+++ lib/classes/subscriptions.dart
@@ -1,5 +1,7 @@
-class Subscriptions
-{
+import 'package:LibreMedia/variables.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Subscriptions {
   static final Subscriptions _instance = Subscriptions._internal();
 
   factory Subscriptions() {
@@ -8,5 +10,21 @@ class Subscriptions
 
   Subscriptions._internal();
 
-  
+  Future<Set<String>> getSubscriptions() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final subs = prefs.getStringList(keySubscriptions) ?? [];
+    return subs.reversed.toSet();
+  }
+
+  Future<void> addToSubscriptions(String channelID) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    Set<String> subs = await getSubscriptions();
+    subs.add(channelID);
+    prefs.setStringList(keySubscriptions, subs.toList());
+  }
+
+  Future<void> clearSubscriptions() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setStringList(keySubscriptions, []);
+  }
 }
