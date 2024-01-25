c4a0dd2d3e98ff792707b25dc543a9585f19780e
Liam Rahum <liam@rahum.co.il>
2024-01-25 10:21:34 +0200

    Build: extracted search bar & results

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,6 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, use_build_context_synchronously, curly_braces_in_flow_control_structures
-
-import 'package:LibreMedia/classes/fetch-data.dart';
 import 'package:flutter/material.dart';
-import 'package:LibreMedia/variables.dart';
 import 'package:LibreMedia/widgets/channel-card.dart';
-import 'package:LibreMedia/widgets/video-card.dart';
+import 'package:LibreMedia/widgets/search-bar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -23,95 +19,18 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const SearchBar(),
+        const CustomSearchBar(),
         Expanded(
           child: ListView(
             physics: const BouncingScrollPhysics(),
-            children: [
-              const FutureChannelCard(channelID: "UCg6gPGh8HU2U01vaFCAsvmQ"),
-              const FutureChannelCard(channelID: "UC5qDet6sa6rODi7t6wfpg8g"),
-              const FutureChannelCard(channelID: "UCux-_Fze30tFuI_5CArwSmg"),
+            children: const [
+              FutureChannelCard(channelID: "UCg6gPGh8HU2U01vaFCAsvmQ"),
+              FutureChannelCard(channelID: "UC5qDet6sa6rODi7t6wfpg8g"),
+              FutureChannelCard(channelID: "UCux-_Fze30tFuI_5CArwSmg"),
             ],
           ),
         ),
       ],
-    );
-  }
-}
-
-class SearchBar extends StatelessWidget {
-  const SearchBar({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-          horizontal: MediaQuery.of(context).size.width * .04),
-      child: TextField(
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          hintText: 'Search',
-          filled: true,
-          fillColor: const Color(0xFF363636),
-          contentPadding: const EdgeInsets.only(
-            left: 14.0,
-            bottom: 6.0,
-            top: 8.0,
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.white),
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          enabledBorder: UnderlineInputBorder(
-            borderSide: const BorderSide(color: Color(0xFF363636)),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-        ),
-        onSubmitted: (prompt) {
-          if(prompt.isEmpty)
-            return;
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => Scaffold(
-                backgroundColor: bgColor,
-                appBar: AppBar(
-                  shadowColor: Colors.transparent,
-                  title: const Text("Search results"),
-                ),
-                body: SearchResultVideoCards(prompt: prompt),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class SearchResultVideoCards extends StatelessWidget {
-  const SearchResultVideoCards({super.key, required this.prompt});
-  final String prompt;
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: fetchVideoList(prompt),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ListView.builder(
-            addAutomaticKeepAlives: true,
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) {
-              return FutureVideoCard(videoId: snapshot.data![index]);
-            },
-          );
-        } else if (snapshot.hasError)
-          return const Text("Error loading videos");
-        else
-          return const LinearProgressIndicator();
-      },
     );
   }
 }
