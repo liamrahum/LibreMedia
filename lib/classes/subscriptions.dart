c64aab9a6576499c78e70006fe25b47b71342db8
Liam Rahum <liam@rahum.co.il>
2024-01-25 10:48:40 +0200

    Build: added subs file, changed watchHistory file name

---
+++ lib/classes/subscriptions.dart
@@ -0,0 +1,12 @@
+class Subscriptions
+{
+  static final Subscriptions _instance = Subscriptions._internal();
+
+  factory Subscriptions() {
+    return _instance;
+  }
+
+  Subscriptions._internal();
+
+  
+}
